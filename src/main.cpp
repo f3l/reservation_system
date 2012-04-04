9f0983b2b0aa7c3c9309b7554d0606d96a740f57
Oliver Rümpelein <oli_r@fg4f.de>
2012-04-04 10:52:34 +0200

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/*Include basic libraries*/
+#include<iostream>
+#include<cstdlib>
+
+/*And own objects*/
+#include<"./room.h">
+usiing namespace std;
+
+int main()
+/*
+ * Creates a new cinema (or else) room with given seats per rows and rows,
+ * and the provides a simple menu for actions like reservations etc.
+ */
+{
+	room=room(); /*Create new object game of type game*/
+	while() 
+		{
+		}
+}
