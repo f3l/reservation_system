294bd339334430204fb6691ffab6c989cc27bd28
Oliver Rümpelein <oli_r@fg4f.de>
2012-04-04 19:28:31 +0200

    Changed main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,16 @@ using namespace std;
 /*And own objects*/
 #include "room.h"
 
-
-int main()
 /*
  * Creates a new cinema (or else) room with given seats per rows and rows,
  * and the provides a simple menu for actions like reservations etc.
  */
+
+int main()
 {
 	room theater; /*Create new object game of type game*/
+	theater.print_room();
+	theater.lock();
 	theater.print_room();
 	return 0;
 }
