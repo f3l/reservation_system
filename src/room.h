ababddc888181e1575d97100cb05652ac4447562
Oliver Rümpelein <oli_r@fg4f.de>
2012-04-04 16:39:46 +0200

    Implemented necessary routines

--- src/room.h
+++ src/room.h
@@ -1,6 +1,9 @@
 /* 
  * Provides a class for reservation in a rectangle-shaped room.
  * To see how it works, and what it provides, see /doc/classes
+ * MAYBE we can do the ijnput in an external function with
+ * pointers/refs (we'll do that later)
+ * Same with reserved-affirmation
  */
 
 typedef unsigned int uint; /*We will need many unsignes ints*/
@@ -235,5 +238,261 @@ void room::unlock()
 	if( seats[trow][tline].state==LOCKED)
 		{
 			seats[trow][tline].state=FREE;
+		}
+}
+
+void room::reserve()
+/*reserve a place for a person, needs the name!*/
+{
+	/*the usual input at first*/
+	uint trow;
+	uint tline;
+	while(true)
+		{
+			cout<<"Which seat should get reserved? (row,line)\n(0 to cancel)"<<endl;
+			cin>>trow;
+			if(trow == 0)
+				{
+					cout<<"Canceled by user request"<<endl;
+					return;
+				}
+			cin>>tline;
+			if(tline == 0)
+				{
+					cout<<"Canceled by user request"<<endl;
+					return;
+				}
+			if(trow<=rows && tline<=lines)
+				{
+					break;
+				}
+		}
+	/*In this case, it's more efficient to first calculate our "machine"-indexes*/
+	trow-=1;
+	tline-=1;
+	
+	/*The seat needs to be in state FREE*/
+	if(!(seats[trow][tline].state==FREE))
+		{
+			cout<<"This seat is not reservable!"<<endl;
+			return;
+		}
+	
+	/*THEN ask for name and save in set new state*/
+	cout<<"For whom should the reservation take place?"<<endl;
+	cin.getline(seats[trow][tline].name,63);
+	seats[trow][tline].state=RESERVED;
+}
+	
+void room::release()
+{
+	/*the usual input at first*/
+	uint trow;
+	uint tline;
+	while(true)
+		{
+			cout<<"The reservation of which seat should get deleted? (row,line)\n(0 to cancel)"<<endl;
+			cin>>trow;
+			if(trow == 0)
+				{
+					cout<<"Canceled by user request"<<endl;
+					return;
+				}
+			cin>>tline;
+			if(tline == 0)
+				{
+					cout<<"Canceled by user request"<<endl;
+					return;
+				}
+			if(trow<=rows && tline<=lines)
+				{
+					break;
+				}
+		}
+	/*In this case, it's more efficient to first calculate our "machine"-indexes*/
+	trow-=1;
+	tline-=1;
+	
+	/*The seat must be Reserved to get released*/
+	if( ! (seats[trow][tline].state==RESERVED))
+		{
+			return;
+		}
+	
+	/*We need an affirmation, we do quite similar to as input*/
+	char affirm='n';
+	while(true)
+		{
+			cout<<"Do you really want to release the reservation of"<<endl;
+			cout<<"seat "<<tline<<" in row "<<trow<<endl;
+			cout<<"currently reserved for"<<endl;
+			for(int i=0;seats[trow][tline].name[i];++i)
+				{
+					cout<<seats[trow][tline].name[i];
+				}
+			cout<<"? (y/N)"<<endl;
+			cin>>affirm;
+			switch(affirm)
+				{
+					/*If user doesn't affirm, we stop the routine*/
+				case 'N':
+				case 'n':
+					return;
+					break;
+					/*If he does, we release the seat and exit*/
+				case 'Y':
+				case 'y':
+					seats[trow][tline].name[0]=0;
+					seats[trow][tline].state=FREE;
+					return;
+					break;
+					/*Otherwise, we give it another try*/
+				}
+		}
+}
+
+void room::handout()
+{
+	/*usual input*/
+	uint trow;
+	uint tline;
+	while(true)
+		{
+			cout<<"Which seat is the handed out ticket for? (row,line)\n(0 to cancel)"<<endl;
+			cin>>trow;
+			if(trow == 0)
+				{
+					cout<<"Canceled by user request"<<endl;
+					return;
+				}
+			cin>>tline;
+			if(tline == 0)
+				{
+					cout<<"Canceled by user request"<<endl;
+					return;
+				}
+			if(trow<=rows && tline<=lines)
+				{
+					break;
+				}
+		}
+	/*In this case, it's more efficient to first calculate our "machine"-indexes*/
+	trow-=1;
+	tline-=1;
+	
+	/*
+	 * THis time, we check whether it is free or reserverd and instantly
+	 * ask for verification in the later case
+	 */
+	if( seats[trow][tline].state==RESERVED)
+		{
+			char affirm='n';
+			while(true)
+				{
+					cout<<"Do you really want to hand out the reservation of"<<endl;
+					cout<<"seat "<<tline<<" in row "<<trow<<endl;
+					cout<<"currently reserved for"<<endl;
+					for(int i=0;seats[trow][tline].name[i];++i)
+						{
+							cout<<seats[trow][tline].name[i];
+						}
+					cout<<"? (y/N)"<<endl;
+					cin>>affirm;
+					
+					/*
+					 * We can't use a switch/case here, because we need
+					 * break for exiting the while-loop
+					 */
+					if(affirm=='y' || affirm =='Y')
+						{
+							break;
+						}
+					else if(affirm=='n' || affirm =='N')
+						{
+							return;
+						}
+					else
+						{
+							cout<<"Invalid input!"<<endl;
+						}
+				}
+		}
+	if( (seats[trow][tline].state==FREE) || (seats[trow][tline].state=RESERVED))
+		{
+			seats[trow][tline].state=HANDED;
+			seats[trow][tline].name[0]=0;
+		}
+	else
+		{
+			cout<<"The seat is not available!"<<endl;
+			return;
+		}
+}
+
+/*Take back cards (almost the same as release)*/
+void room::cancel()
+{
+	/*the usual input at first*/
+	uint trow;
+	uint tline;
+	while(true)
+		{
+			cout<<"The card of which seat should be taken back? (row,line)\n(0 to cancel)"<<endl;
+			cin>>trow;
+			if(trow == 0)
+				{
+					cout<<"Canceled by user request"<<endl;
+					return;
+				}
+			cin>>tline;
+			if(tline == 0)
+				{
+					cout<<"Canceled by user request"<<endl;
+					return;
+				}
+			if(trow<=rows && tline<=lines)
+				{
+					break;
+				}
+		}
+	/*In this case, it's more efficient to first calculate our "machine"-indexes*/
+	trow-=1;
+	tline-=1;
+	
+	/*The seat must be HANDED to get released*/
+	if( ! (seats[trow][tline].state==HANDED))
+		{
+			return;
+		}
+	
+	/*We need an affirmation, we do quite similar to as input*/
+	char affirm='n';
+	while(true)
+		{
+			cout<<"Do you really want to cancel the card for"<<endl;
+			cout<<"seat "<<tline<<" in row "<<trow<<endl;
+			cout<<"currently reserved for"<<endl;
+			for(int i=0;seats[trow][tline].name[i];++i)
+				{
+					cout<<seats[trow][tline].name[i];
+				}
+			cout<<"? (y/N)"<<endl;
+			cin>>affirm;
+			switch(affirm)
+				{
+					/*If user doesn't affirm, we stop the routine*/
+				case 'N':
+				case 'n':
+					return;
+					break;
+					/*If he does, we release the seat and exit*/
+				case 'Y':
+				case 'y':
+					seats[trow][tline].name[0]=0;
+					seats[trow][tline].state=FREE;
+					return;
+					break;
+					/*Otherwise, we give it another try*/
+				}
 		}
 }
