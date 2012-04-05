4aa41123b4b4f1cd3c0c07d63a3e4417259e1fc3
Oliver Rümpelein <oli_r@fg4f.de>
2012-04-05 20:34:55 +0200

    Added licensing information

--- src/room.cpp
+++ src/room.cpp
@@ -1,3 +1,28 @@
+/*
+ *  Reservation System - A programm to manage rect-shaped seatings
+ *  Copyright (C) 2012 Johannes Loher / Oliver Rümpelein
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Contact: ghost91@gmx.de or oli_r@fg4f.de
+ *  Get current code at <http://www.github.com/f3l/reservation_system>
+ */
+
+/*
+ *  The routines for class room from "room.h"
+ */
+
 #include <iostream>
 #include <string>
 #include <limits>
